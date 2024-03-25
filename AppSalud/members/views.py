4d1712f3d84d1e9660e3ef1b1afc60775b29bb5e
Manuel Ramirez <manoloramirez.mrv@gmail.com>
2024-03-25 17:41:21 +0100

    Creando templates all membres

--- AppSalud/members/views.py
+++ AppSalud/members/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import MiModelo
 
 def members(request):
-  template = loader.get_template('myfirst.html')
-  return HttpResponse(template.render())
-
+  mymembers = MiModelo.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
 # Create your views here.
